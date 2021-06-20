66155eb296fcbcea899f692ea59562a7f62f1561
Alexander Katchkatchishvili <alextechstart@gmail.com>
2021-06-20 08:15:14 +0400

    init

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  tabWidth: 2,
+  semi: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'always',
+};
